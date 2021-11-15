76df772b813fa9ce9270abc6abc8abf5eda6865d
kimseokjin <kimseokjin0324@gmail.com>
2021-11-16 00:02:31 +0900

    오류코드와 메시지 처리5- 오류메시지 레벨에따른 처리와 검증로직에 대한 간단한 코드작성 예시

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -11,6 +11,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -145,9 +146,11 @@ public class ValidationItemControllerV2 {
         log.info("target={}", bindingResult.getTarget());
 
         //검증로직
-        if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.rejectValue("itemName","required");
-        }
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult,"itemName","required");
+//        if (!StringUtils.hasText(item.getItemName())) {
+//            bindingResult.rejectValue("itemName","required");
+//
+//        } //- 위 코드와 동일
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.rejectValue("price","range",new Object[]{1000,1000000},null);
         }
